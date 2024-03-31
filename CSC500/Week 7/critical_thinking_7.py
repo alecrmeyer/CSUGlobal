454f5cb4d39674ac8eac6fbb085d258eb501fa19
Alec Meyer <alecrmeyer4@gmail.com>
2024-03-31 09:34:42 -0600

    Critical Thinking 7

---
+++ CSC500/Week 7/critical_thinking_7.py
@@ -0,0 +1,29 @@
+room_number = {
+    "CSC101": 3004,
+    "CSC102": 4501,
+    "CSC103": 6755,
+    "NET110": 1244,
+    "COM241": 1411
+}
+
+instructors = {
+    "CSC101": "Haynes",
+    "CSC102": "Alvarado",
+    "CSC103": "Rich",
+    "NET110": "Burke",
+    "COM241": "Lee"
+}
+
+meeting_time = {
+    "CSC101": "8:00 a.m.",
+    "CSC102": "9:00 a.m.",
+    "CSC103": "10:00 a.m.",
+    "NET110": "11:00 a.m.",
+    "COM241": "1:00 p.m."
+}
+
+attribute_list = [room_number, instructors, meeting_time]
+
+course = input("What course would you like information about? ")
+for attribute in attribute_list:
+    print(attribute[course])

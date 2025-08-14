230137184c3c1e5b34310399edfd866ee249a120
Alec Meyer <alecrmeyer4@gmail.com>
2025-08-13 19:54:59 -0600

    First critical thinking assignment

--- CSC510/Week4/Crtitical_Thinking_4.py
+++ CSC510/Week4/Crtitical_Thinking_4.py
@@ -1,17 +1,23 @@
 from simpleai.search import astar, SearchProblem
 from simpleai.search.viewers import WebViewer
-import random
-
-WEIGHTS = [25, 35, 45]
 
 GOAL = '''25-25-e-e
 35-35-e-e
 45-45-e-e'''
 
-INITIAL = '''25-e-e-e
+INITIAL_1 = '''25-e-e-e
 25-35-45-e
 45-35-e-e'''
 
+INITIAL_2 = '''45-35-e-e
+45-e-e-e
+25-35-25-e'''
+
+INITIAL_3 = '''35-25-45-e
+e-e-e-e
+25-35-45-e'''
+
+INITIAL_LIST = [INITIAL_1, INITIAL_2, INITIAL_3]
 
 
 def list_to_string(list_):
@@ -20,10 +26,7 @@ def list_to_string(list_):
 def string_to_list(string_):
     return [row.split('-') for row in string_.split('\n')]
 
-def generate_random():
-    num_weight = random.randint(1,6)
     
-
 class EigthPuzzleProblem(SearchProblem):
    
     def actions(self, state):
@@ -84,13 +87,62 @@ class EigthPuzzleProblem(SearchProblem):
 
         return cost
 
+###### User Interface ######
+initial = ""
+while(True):
+    print("Would you like to use a precoded initial state (p) or create a custom state? (c)")
+    answer = input("(p) or (c)?: ")
+    if answer == 'p':
+        print("please choose from the precoded initial states")
+        print("Option 1\n" + INITIAL_1 + "\n")
+        print("Option 2\n" + INITIAL_2 + "\n")
+        print("Option 3\n" + INITIAL_3 + "\n")
+        while(True):
+            choice = input("Please type the number for the desired option: ")
+            choice = int(choice)
+            if choice <= len(INITIAL_LIST) and choice > 0:
+                initial = INITIAL_LIST[choice - 1]
+                break
+            print("ERROR: Please enter \'1\', \'2\' or \'3\'")
+        break
+    elif answer == 'c':
+        while(True):
+            print("please enter an initial state where you have (2) 25 pound weights, (2) 35 pound weights and (2) 45 pound weights. Put a space between each weight (e.g. 25 35 35)")
+            rung_1 = input("Enter the order of weights on the first rung: ")
+            rung_2 = input("Enter the order of weights on the second rung: ")
+            rung_3 = input("Enter the order of weights on the third rung: ")
 
+            rung_1 = rung_1.split()
+            rung_2 = rung_2.split()
+            rung_3 = rung_3.split()
 
-result = astar(EigthPuzzleProblem(INITIAL))
-# if you want to use the visual debugger, use this instead:
-# result = astar(EigthPuzzleProblem(INITIAL), viewer=WebViewer())
+            if (len(rung_1) + len(rung_2) + len(rung_3)) == 6:
+                def fill_with_e(rung):
+                    while(len(rung)<4):
+                        rung.append('e')
+                    return rung
+                fill_with_e(rung_1)
+                fill_with_e(rung_2)
+                fill_with_e(rung_3)
 
+                initial_list = []
+                initial_list.append(rung_1)
+                initial_list.append(rung_2)
+                initial_list.append(rung_3)
+                initial = list_to_string(initial_list)
+                break
+            print("ERROR: Please enter exactly 6 weights where (2) are 25, (2) are 35 and (2) are 45")
+        break
+
+    else:
+        print("ERROR: Please enter \'p\' or \'c\'")
+
+result = astar(EigthPuzzleProblem(initial))
+
+count = 0
 for action, state in result.path():
-    print('Move number', action)
+    print('Move number ' + str(count))
+    count+=1
     print(state)
+    print()
 
